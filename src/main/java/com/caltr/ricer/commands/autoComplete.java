90594202d83b4dc77ed13e688ceb8968827c15fe
Catsef <96400367+Catsef@users.noreply.github.com>
2024-02-04 19:51:21 +0800

    all these dawgs;;;;;;

--- src/main/java/com/caltr/ricer/commands/autoComplete.java
+++ src/main/java/com/caltr/ricer/commands/autoComplete.java
@@ -1,4 +1,31 @@
 package com.caltr.ricer.commands;
 
-public class autoComplete {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class autoComplete implements TabCompleter {
+
+    @Override
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String s, String[] strings) {
+        List<String> autocompleteOptions = new ArrayList<>();
+        if (command.getName().equalsIgnoreCase("item") && strings.length == 0) {
+            if (commandSender instanceof Player) {
+                Player player = (Player) commandSender;
+                autocompleteOptions.add("lifter");
+                autocompleteOptions.add("luigi-flashlight");
+                autocompleteOptions.add("punch-combo");
+                autocompleteOptions.add("punch");
+                autocompleteOptions.add("totem-of-dying");
+                autocompleteOptions.add("cursed-helmet");
+
+            }
+        }
+
+        return autocompleteOptions;
+    }
 }
